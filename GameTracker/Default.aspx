563c7267b3a7b936db9f7e8f6fa6b1a4cf85b10d
averyirin <averyirin@gmail.com>
2016-06-24 17:46:41 -0400

    Added Default Page

---
+++ GameTracker/Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="GameTracker.Default" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+     <div class="container">
+        <div class="row">
+            <div class="col-md-offset-2 col-md-8">
+                <h1>Game List</h1>
+
+                        <asp:GridView runat="server" CssClass="table table-bordered table-striped table-hover"
+                    ID="GamesGridView" AutoGenerateColumns="false" DataKeyNames="GameID"
+                    OnRowDeleting="GamesGridView_RowDeleting" AllowSorting="true"
+                    OnSorting="GamesGridView_Sorting" OnRowDataBound="GamesGridView_RowDataBound" 
+                    PagerStyle-CssClass="pagination-ys">
+                    <Columns>
+                        <asp:BoundField DataField="GameID" HeaderText="Game ID" Visible="true" SortExpression="GameID" />
+                        <asp:BoundField DataField="Description" HeaderText="Description" Visible="true" SortExpression="Description" />
+                        <asp:BoundField DataField="Spectators" HeaderText="Spectators" Visible="true" SortExpression="Spectators" />
+                        <asp:BoundField DataField="DatePlayed" DataFormatString="{0:MMM dd, yyyy}"  HeaderText ="Date Played" Visible="true" SortExpression="DatePlayed" />
+                       
+                    </Columns>
+                </asp:GridView>
+            </div>
+        </div>
+    </div>
+    
+    
+</asp:Content>
---
+++ GameTracker/Default.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+//required to connect to EF db
+using GameTracker.Models;
+using System.Web.ModelBinding;
+namespace GameTracker
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e) {
+            // if loading the page for the first time, populate the student grid
+            if (!IsPostBack) {
+                Session["SortColumn"] = "GameID"; // default sort column
+                Session["SortDirection"] = "ASC";
+                // Get the student data
+                this.GetGames();
+            }
+        }
+
+        /**
+         * <summary>
+         * This method gets the student data from the DB
+         * </summary>
+         * 
+         * @method GetGames
+         * @returns {void}
+         */
+        protected void GetGames() {
+            // connect to EF
+            using (GameTrackerConn db = new GameTrackerConn()) {
+                string SortString = Session["SortColumn"].ToString() + " " + Session["SortDirection"].ToString();
+
+                // query the Gridview_template_item_view1 Table using EF and LINQ
+                var GamesList = (from allGames in db.Games
+                                   select allGames);
+
+                // bind the result to the GridView
+                GamesGridView.DataSource = GamesList.AsQueryable().OrderBy(SortString).ToList();
+                GamesGridView.DataBind();
+            }
+        }
+
+        
+
+        protected void GamesGridView_Sorting(object sender, GridViewSortEventArgs e) {
+            // get the column to sorty by
+            Session["SortColumn"] = e.SortExpression;
+
+            // Refresh the Grid
+            this.GetGames();
+
+            // toggle the direction
+            Session["SortDirection"] = Session["SortDirection"].ToString() == "ASC" ? "DESC" : "ASC";
+        }
+
+        protected void GamesGridView_RowDataBound(object sender, GridViewRowEventArgs e) {
+            if (IsPostBack) {
+                if (e.Row.RowType == DataControlRowType.Header) // if header row has been clicked
+                {
+                    LinkButton linkbutton = new LinkButton();
+
+                    for (int index = 0; index < GamesGridView.Columns.Count - 1; index++) {
+                        if (GamesGridView.Columns[index].SortExpression == Session["SortColumn"].ToString()) {
+                            if (Session["SortDirection"].ToString() == "ASC") {
+                                linkbutton.Text = " <i class='fa fa-caret-up fa-lg'></i>";
+                            }
+                            else {
+                                linkbutton.Text = " <i class='fa fa-caret-down fa-lg'></i>";
+                            }
+
+                            e.Row.Cells[index].Controls.Add(linkbutton);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
